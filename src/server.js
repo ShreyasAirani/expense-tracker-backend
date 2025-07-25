0a428adc240137cb45012ff09e1c09d1f98cc79c
ShreyasAirani <shreyas8631@gmail.com>
2025-07-25 15:54:16 +0530

    user login error fixed

--- src/server.js
+++ src/server.js
@@ -40,6 +40,7 @@ app.use(cors({
     'http://127.0.0.1:5173',
     'http://127.0.0.1:5174',
     'http://127.0.0.1:5175',
+    'https://ezspend.vercel.app',
     process.env.FRONTEND_URL
   ].filter(Boolean),
   credentials: true,
